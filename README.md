683b2d07fb0d598c27f9564f274afab9f9eb032d
Robin ten Berge <rberge.nl@gmail.com>
2018-05-07 16:35:19 +0000

    initial setup using a mock for pigpio

---
+++ README.md
@@ -0,0 +1,6 @@
+https://github.com/novemberalpha/openag_brain_pfc1_config
+
+
+1. sudo apt-get update
+2. sudo apt-get install pigpio
+3. npm install pigpio
---
+++ test/sensors.js
@@ -0,0 +1,23 @@
+// IN
+let air_temperature
+let air_humidity;
+let air_carbon_dioxide;
+let water_level;
+let lumen;
+let electric_conductivity;
+let water_potential_hydrogen;
+
+
+// OUT
+let air_flush_on; // fan exhaust
+let fan_airflow;
+let water_pump;
+let air_pump;
+let light_intensity_blue;
+let light_intensity_red;
+let light_intensity_white;
+let nutrient_flora_duo_a;
+let nutrient_flora_duo_b;
+let ph_down_pump;
+let co2_pump;
+let dehumidifier;
